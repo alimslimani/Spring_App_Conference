39db65e9a249229fe239614a0faebbfd1c1c1dda
alimslimani <slimanialim@yahoo.fr>
2019-12-18 12:07:12 +0100

    alm-001: Add factoryBean configuration

--- src/main/java/com/alm/highfi/config/AppConfig.java
+++ src/main/java/com/alm/highfi/config/AppConfig.java
@@ -4,15 +4,32 @@ import com.alm.highfi.repository.HibernateSpeakerRepositoryImpl;
 import com.alm.highfi.repository.SpeakerRepository;
 import com.alm.highfi.service.SpeakerService;
 import com.alm.highfi.service.SpeakerServiceImpl;
+import com.alm.highfi.util.CalendarFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.util.Calendar;
+
 @Configuration
 @ComponentScan({"com.alm.highfi"})
 public class AppConfig {
+
+    @Bean(name = "cal")
+    public CalendarFactory calFactory() {
+        CalendarFactory factory = new CalendarFactory();
+        factory.addDays(2);
+        return factory;
+    }
+
+    @Bean
+    public Calendar cal() throws Exception {
+        return calFactory().getObject();
+    }
+
+
 // unused after adding @ComponentScan and other streotype @Service & @Repository
     /*
     @Bean(name = "speakerService")
--- src/main/java/com/alm/highfi/service/SpeakerServiceImpl.java
+++ src/main/java/com/alm/highfi/service/SpeakerServiceImpl.java
@@ -3,11 +3,14 @@ package com.alm.highfi.service;
 import com.alm.highfi.model.Speaker;
 import com.alm.highfi.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service("speakerService")
+@Profile("dev")
 public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
@@ -22,6 +25,12 @@ public class SpeakerServiceImpl implements SpeakerService {
         System.out.println("SpeakerServiceImpl repository constructor");
         repository = speakerRepository;
     }
+
+    @PostConstruct
+    private void initialize() {
+        System.out.println("We're called after constructors");
+    }
+
 
     /*  With Setter Injection   */
 //    @Autowired
